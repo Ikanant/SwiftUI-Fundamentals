cec610c9f07619f7bdbe094a4bd57acd440ae87a
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-14 19:11:22 -0400

    Initial Commit

---
+++ SwiftUI Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Jonathan Hernandez on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
