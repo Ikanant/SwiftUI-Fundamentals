492c384a55ec026f1ccd315c363582188b428462
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-15 20:42:05 -0400

    Playing around with Spacers and how they can move my VIEW(s) (in this case simple text) around the display area of my application

--- SwiftUI Fundamentals/ContentView.swift
+++ SwiftUI Fundamentals/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI")
-            .padding()
+        HStack {
+            Spacer()
+            VStack {
+                Text("Hello From the TOP RIGHT")
+                    .padding()
+                Spacer()
+            }
+        }
     }
 }
 
